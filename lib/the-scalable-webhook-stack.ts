5e89edd0de92019513402448dd7059a02cd5797b
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-04 20:59:48 +0500

    Create a DynamoDB table

--- lib/the-scalable-webhook-stack.ts
+++ lib/the-scalable-webhook-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
@@ -9,5 +10,18 @@ import * as sqs from "aws-cdk-lib/aws-sqs";
 export class TheScalableWebhookStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // =============================================================================
+    /**
+     * Create a DynamoDB table
+     * this is standing in for what RDS on the diagram due to simple/cheaper setup
+     */
+    const table = new dynamodb.Table(this, "Table", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      tableName: "the-scalable-webhook-table",
+      writeCapacity: 1,
+      readCapacity: 1,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
